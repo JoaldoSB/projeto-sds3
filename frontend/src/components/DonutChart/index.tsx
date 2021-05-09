a4ae1f131efe1e2e3be0d1506c5bf86054df189e
Joaldo Santos <joaldo.einstein@hotmail.com>
2021-05-09 19:34:32 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import Chart from 'react-apexcharts'
+import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
@@ -21,26 +21,22 @@ const DonutChart = () => {
                 const mySeries = data.map(x => x.sum);
 
                 setChartData({ labels: myLabels, series: mySeries });
-             });
+            });
     }, []);
-        //const mockData = {
-        //    series: [477138, 499928, 444867, 220426, 473088],
-        //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        //}
 
-        const options = {
-            legend: {
-                show: true
-            }
+    const options = {
+        legend: {
+            show: true
         }
-        return (
-            <Chart
-                options={{ ...options, labels: chartData.labels }}
-                series={chartData.series}
-                type="donut"
-                height="240"
-            />
-        );
     }
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
+    );
+}
 
 export default DonutChart;
